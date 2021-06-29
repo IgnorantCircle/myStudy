13a2b8b970ec759a194edec2ca3cfd67b9d9961c
Ignorant-Circle <df314376@126.com>
2021-06-29 16:16:16 +0800

    做了一点小修改，增加了readme的内容

--- README.md
+++ README.md
@@ -2,7 +2,17 @@
 some study notes and demo 
 # demo
 里面放的是笔记中的一些实例代码
+
+类型：
+
+- ajax
+- axios
+- promise
+- React
+- Redux
+
 # notes
 里面是一些学习笔记
 # learning_materials
 里面是一些学习资料
+
--- demo/React/react_staging/src/App.jsx
+++ demo/React/react_staging/src/App.jsx
@@ -1,31 +1,24 @@
 import React, { Component } from 'react'
-import { Button,DatePicker } from 'antd';
-import {
-    WechatOutlined,
-    SearchOutlined
-   
-  } from '@ant-design/icons';
+import Searcher from './component/Search/index'
+import List from './component/List/index';
 export default class App extends Component {
-     onChange(date, dateString) {
-        console.log(date, dateString);
-      }
+    state = {
+        users:[],//初始化状态，初始值为数组
+        isFirst:true,//是否为第一次打开页面
+        isLoading:false,//标识是否处于加载中
+        err:'',//存储请求相关的错误信息
+    } 
+    //更新App的state
+   updateAppState = (stateObj)=> {
+        this.setState(stateObj);
+    }
+  
     render() {
         return (
-            <div>
-             APP...
-            <button>点我</button>
-            <Button type="primary">Primary Button</Button>
-            <Button type="link">link</Button>
-            <Button type="ghost">link</Button>
-            <Button type="dashed">link</Button>
-            <WechatOutlined style={{fontSize:100}} />
-            <Button type="primary" icon={<SearchOutlined />}>
-                Search
-            </Button>
-            <DatePicker onChange={this.onChange} />
-            
-               
-        </div>
+            <div className="container">
+             <Searcher updateAppState={this.updateAppState}/>
+             <List {...this.state}/>
+          </div>
         )
     }
 }
